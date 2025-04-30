50a45cf07c50000f6fa9923fba246c00484559e6
eliseuqueiroz <eliseuqueiroz064@gmail.com>
2025-04-30 16:59:14 -0300

    deu certo meu codigo kkk

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+
 //projeto super heroi do desafio do felipão, "nivél boos"
+
+// Variáveis do herói
+let nomeHeroi = "Super Eliseu";
+let xpHeroi = 5000;
+let nivel = "";
+
+// Estrutura de decisão para classificar o nível
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else if (xpHeroi >= 10001) {
+    nivel = "Radiante";
+}
+
+// Exibição da mensagem final 
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
